c1ef6fd7707f54ab2ee79becd3f2b1d3e466a1a7
Volodymyr Horin <vladimir.horin@magicpowered.io>
2021-12-17 17:48:36 +0200

    feat(auth jwt): added near login + near connect wallet

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,5 +3,7 @@ import { registerAs } from '@nestjs/config';
 export const app = registerAs('app', () => {
   return {
     port: parseInt(process.env.PORT, 10),
+    frontendUrl: process.env.FRONTEND_URL,
+    jwtSecret: process.env.JWT_SECRET,
   };
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { User, UserDocument } from 'src/schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import { UserDto } from './dto/user.dto';
 
 @Injectable()
@@ -14,7 +14,22 @@ export class UserService {
     return model.save();
   }
 
-  async findByAccount(accountId: string): Promise<UserDocument> {
-    return this.userModel.findOne({ accountId }).exec();
+  async addNearAccount(
+    userId: string,
+    accountId: string,
+  ): Promise<UserDocument> {
+    const model = await this.findById(userId);
+    model.nearAccounts = [...model.nearAccounts, accountId];
+    return model.save();
+  }
+
+  async findByNearAccount(accountId: string): Promise<UserDocument> {
+    return this.userModel
+      .findOne({ nearAccounts: { $in: [accountId] } })
+      .exec();
+  }
+
+  async findById(userId: string): Promise<UserDocument> {
+    return this.userModel.findById(userId).exec();
   }
 }
